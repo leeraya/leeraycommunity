9063d695086ddae1d0bc52f3f326f12cab56675d
studiolee <1964773741@qq.com>
2019-08-03 20:55:19 +0800

    完成图片上传

--- src/main/java/life/leeray/community/controller/FileController.java
+++ src/main/java/life/leeray/community/controller/FileController.java
@@ -1,8 +1,15 @@
 package life.leeray.community.controller;
 
 import life.leeray.community.dto.FileDTO;
+import life.leeray.community.provider.UCloudProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author leeray
@@ -12,10 +19,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class FileController {
 
-    @RequestMapping(value = "/file/upload")
-    public FileDTO upload() {
-        FileDTO fileDTO = new FileDTO();
+    @Autowired
+    private UCloudProvider uCloudProvider;
 
+    @RequestMapping(value = "/file/upload")
+    @ResponseBody
+    public FileDTO upload(HttpServletRequest request) {
+        FileDTO fileDTO = new FileDTO();
+        MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+        MultipartFile file = multipartRequest.getFile("editormd-image-file");
+        try {
+            String imgUrl = uCloudProvider.upload(file.getInputStream(), file.getContentType(), file.getOriginalFilename());
+            fileDTO.setSuccess(1);
+            fileDTO.setUrl(imgUrl);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return fileDTO;
     }
 }
---
+++ src/main/java/life/leeray/community/provider/UCloudProvider.java
@@ -0,0 +1,80 @@
+package life.leeray.community.provider;
+
+import cn.ucloud.ufile.UfileClient;
+import cn.ucloud.ufile.api.object.ObjectConfig;
+import cn.ucloud.ufile.auth.ObjectAuthorization;
+import cn.ucloud.ufile.auth.UfileObjectLocalAuthorization;
+import cn.ucloud.ufile.bean.PutObjectResultBean;
+import cn.ucloud.ufile.exception.UfileClientException;
+import cn.ucloud.ufile.exception.UfileServerException;
+import life.leeray.community.exception.CustomizeErrorCode;
+import life.leeray.community.exception.CustomizeException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.InputStream;
+import java.util.UUID;
+
+/**
+ * @author leeray
+ * @version 1.0
+ * @date 2019/8/3 0003 16:58
+ * ucloud上传文件
+ */
+@Service
+public class UCloudProvider {
+    @Value("${ucloud.ufile.public-key}")
+    private String publicKey;
+
+    @Value("${ucloud.ufile.private-key}")
+    private String privateKey;
+
+    @Value("${ucloud.ufile.bucket-name}")
+    private String bucketName;
+
+    @Value("${ucloud.ufile.expires}")
+    private Integer expires;
+
+    @Value("${ucloud.ufile.region}")
+    private String region;
+
+    @Value("${ucloud.ufile.proxy-suffix}")
+    private String suffix;
+
+
+    public String upload(InputStream fileStream, String mimeType, String fileName) {
+        String generatedFileName;
+        String[] filepaths = fileName.split("\\.");
+        if (filepaths.length > 1) {
+            generatedFileName = UUID.randomUUID().toString() + "." + filepaths[filepaths.length - 1];
+        } else {
+            throw new CustomizeException(CustomizeErrorCode.FILE_UPLOAD_FAIL);
+        }
+        try {
+            ObjectAuthorization objectAuthorization = new UfileObjectLocalAuthorization(publicKey, privateKey);
+            ObjectConfig config = new ObjectConfig(region, suffix);
+            PutObjectResultBean response = UfileClient.object(objectAuthorization, config)
+                    .putObject(fileStream, mimeType)
+                    .nameAs(generatedFileName)
+                    .toBucket(bucketName)
+                    .setOnProgressListener((bytesWritten, contentLength) -> {
+
+                    })
+                    .execute();
+            if (response != null && response.getRetCode() == 0) {
+                String url = UfileClient.object(objectAuthorization, config)
+                        .getDownloadUrlFromPrivateBucket(generatedFileName, bucketName, expires)
+                        .createUrl();
+                return url;
+            } else {
+                throw new CustomizeException(CustomizeErrorCode.FILE_UPLOAD_FAIL);
+            }
+        } catch (UfileClientException e) {
+            e.printStackTrace();
+            throw new CustomizeException(CustomizeErrorCode.FILE_UPLOAD_FAIL);
+        } catch (UfileServerException e) {
+            e.printStackTrace();
+            throw new CustomizeException(CustomizeErrorCode.FILE_UPLOAD_FAIL);
+        }
+    }
+}
